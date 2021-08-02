4732afba3293e70bc715ce3753c086e87a1e443f
lucas.novak <lucas.novak@fesctecnologia.com.br>
2021-08-02 19:39:07 -0300

    mudanca

--- src/main/resources/db/migration/V002__create_table_cliente.sql
+++ src/main/resources/db/migration/V002__create_table_cliente.sql
@@ -2,6 +2,11 @@ CREATE TABLE cliente (
 	id 			    bigint not null auto_increment,
 	nome 		    varchar(50) not null,
     cpf             varchar(13) not null,
+    endereco_id     bigint,
     
     primary key(id)
 ) engine=InnoDB default charset=utf8;
+
+ALTER TABLE cliente
+    ADD CONSTRAINT endereco_id_fkey
+        FOREIGN KEY (endereco_id) REFERENCES endereco (id);
